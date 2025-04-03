b64d06da1a1a1f1bacd04cbbe0d16da9123796bb
Giovany de Farias Lima <farias.giovany@gmail.com>
2025-04-03 11:17:41 -0300

    Frontend feito sem detalhes, falta dockerizar o front

--- frontend/frontlistatarefas/app/components/ListaFazer.tsx
+++ frontend/frontlistatarefas/app/components/ListaFazer.tsx
@@ -4,68 +4,79 @@ import React, { useState, useEffect } from 'react';
 import { Tarefas } from '../Tipos';
 import { stringify } from 'querystring';
 import { json } from 'stream/consumers';
-import { useRouter } from 'next/navigation';
+
 
 export default function Listarfazer() {
 
     const [afazer, setAfazer] = useState<Tarefas[]>([]);
     const [cfazendo, setCfazendo] = useState<Tarefas[]>([]);
     const [feitas, setFeitas] = useState<Tarefas[]>([]);
-    const Router = useRouter();
+    const [total,setTotal] = useState<Tarefas[]>([]);
 
   useEffect(() => {
     
     getData([afazer, setAfazer],"listafazer");
     getData([cfazendo, setCfazendo],"listafazendo");
     getData([feitas, setFeitas],"listafeito");
+    getData([total, setTotal],"lista");
   
   }, []);
   async function enviadados([uso, setUso] = useState<Tarefas[]>([]),id: number, destino:number) {
     Alterarestado([uso, setUso],id, destino);
-    if (destino === 0) {
+    if (destino === 0) {   
       const updatedPosts = uso.filter((post) => post.id !== id);
       setUso(updatedPosts);
       const updatedPosts2 = uso.filter((post) => post.id === id);
       setAfazer([...afazer, ...updatedPosts2]);
-      Router.push('/');
+      
     }
     if (destino === 1) {
       const updatedPosts = uso.filter((post) => post.id !== id);
       setUso(updatedPosts);
       const updatedPosts2 = uso.filter((post) => post.id === id);
       setCfazendo([...cfazendo, ...updatedPosts2]);
-      Router.push('/');
+      
     }
     if (destino === 2) {
       const updatedPosts = uso.filter((post) => post.id !== id);
       setUso(updatedPosts);
       const updatedPosts2 = uso.filter((post) => post.id === id);
       setFeitas([...feitas, ...updatedPosts2]);
-      Router.push('/');
+      
+    }
+    if (destino === 3) {
+      const updatedPosts = uso.filter((post) => post.id !== id);
+      setUso(updatedPosts);
     }
     
   }
-  async function fazendo(id:number) {
-    console.log(id);
-    const tarefa = afazer.find(post => post.id === id)?.tarefa;
-    const content = JSON.stringify({ lista: tarefa, status: 1 });
-    console.log(content);
-    const response = await fetch(`http://192.168.0.3:8000/lista/${id}`, {
-      method: 'PUT',
+  async function adicionartarefa([posts, setPosts] = useState<Tarefas[]>([]),tarefa: string){
+    const content= JSON.stringify({ lista: tarefa, status: 0 });
+    const response = await fetch(`http://192.168.0.3:8000/lista/`, {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       }, body: content
       });
-      const updatedPosts = afazer.filter((post) => post.id !== id);
-      console.log(updatedPosts);
-      setAfazer(updatedPosts);
-    }
+      var i=findMaxId(total);
+     
+    setTotal([...total, {tarefa: tarefa, status:0, id: i+1}]);
+    setPosts([...posts, {tarefa: tarefa, status:0, id: i+1}]);
+
+  }
   
 
   return (
   
      <div>
-      
+      <div>
+        <label >Nova tarefa:</label>
+        <input type="text" id="taskInput" placeholder="escreva a nova tarefa" />
+        <button onClick={() => {
+          const inputElement = document.getElementById('taskInput') as HTMLInputElement;
+          if (inputElement) adicionartarefa([afazer,setAfazer],inputElement.value);
+        }}>adicionar tarefa</button>
+      </div>
       <table border={5}>
         <thead>
           <tr>
@@ -152,4 +163,9 @@ export async function Alterarestado([afazer, setAfazer] = useState<Tarefas[]>([]
     }, body: content
     });
     
+  }
+
+  function findMaxId(tasks: Tarefas[]): number {
+    if (tasks.length === 0) return 0; // Return 0 if the list is empty
+    return Math.max(...tasks.map((task) => task.id));
   }
